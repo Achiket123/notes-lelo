33ba717804028d230aa8c842e5536dd3e74e3300
Achiket <achiketkuma@gmail.com>
2023-03-27 22:20:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,177 @@
+Chemistry11 = {
+    'Some Basic Concept of Chemistry': 'https://drive.google.com/file/d/1S1Gbc0RNUQsZuvacCkxJbG67mb_mmhv7/view'
+, 'Structure of Atom': 'https://drive.google.com/file/d/1BIkoJrigZApV31AhCXTeBKuDWc1Oj4vY/view?usp=sharing'
+, 'Classification of Elements and Periodicity in Properties': 'https://drive.google.com/file/d/1BlaYiU2U_6sQnRX9VnBa1nuk_LsxFjBT/view'
+    , 'Chemical Bonding and Molecular Structure': 'https://drive.google.com/file/d/1caoS_rw8Wb2nyFtdfQ0_qN0T7znWZp7B/view'
+    , 'States of Matter': 'https://drive.google.com/file/d/13ecxs-nQN2qsZ5fmDgU-3ufCzdNBuJiV/view'
+    , 'Thermodynamics': 'https://drive.google.com/file/d/1UMOGjxR1Tiz7NYYAwj4sth4GOYmkOfVM/view'
+    , 'Equilibrium': 'https://drive.google.com/file/d/1yP5BrbP64RQCeQRmESEnyQBtM-YGYNlF/view'
+    , 'Redox Reactions ( Password 4321)': 'https://drive.google.com/file/d/1mSVZKhKkbNk-YP35b2HIrKQqkAE0JIcY/view'
+    , 'Hydrogen (Password 5678)': 'https://drive.google.com/file/d/163hPMLXLtlUqOAhn0y6Dzb-dt8lzJQ7d/view'
+    , 'The s-Block Elements (Password 5862)': 'https://drive.google.com/file/d/10bQcHUJIFNaSSrE8KkTf2E93AOtaaGkn/view'
+    , 'The p-Block Elements (Password 2536)': 'https://drive.google.com/file/d/1zjFxV9W2pYg7ea2PHrlf85hvEhWPTRU-/view'
+    , 'Organic Chemistry - Some Basic Principles & Techniques': 'https://drive.google.com/file/d/1kqkfBTajumguVNij-7E1rkH0QJ6ZNqfb/view'
+    , 'Hydrocarbons (Password 3690)': 'https://drive.google.com/file/d/1judSoD3tKPMkRt95vpGKd4MQ5fJI6c6t/view'
+    , 'Summary of All Chapters': 'https://drive.google.com/file/d/1TnAbmkNteAJIdvFHtEnYhAFdDqCBkUmB/view'
+
+
+
+}
+Physics11 = {
+    'Physical World': 'not_uploaded.html'
+    , 'Units and Measurements': 'not_uploaded.html'
+    , 'Motion in a Straight Line': 'not_uploaded.html'
+    , 'Motion in a Plane': 'not_uploaded.html'
+    , 'Laws of Motion': 'not_uploaded.html'
+    , 'Work, Energy and Power': 'not_uploaded.html'
+    , 'System of Particles and Rotational Motion': 'not_uploaded.html'
+    , 'Gravitation': 'not_uploaded.html'
+    , 'Mechanical Properties of Solids': 'not_uploaded.html'
+    , 'Mechanical Properties of Fluids': 'not_uploaded.html'
+    , 'Thermal Properties of Matter': 'not_uploaded.html'
+    , 'Thermodynamics': 'not_uploaded.html'
+    , 'Kinetic Theory': 'not_uploaded.html'
+    , 'Oscillations': 'not_uploaded.html'
+    , 'Waves': 'not_uploaded.html'
+    , 'Physical World': 'not_uploaded.html'
+
+}
+Maths11 = {
+    'Sets': 'not_uploaded.html'
+    , 'Relations & Functions': 'not_uploaded.html'
+    , 'Principle of Mathematical Induction': 'not_uploaded.html'
+    , 'Trignometric Functions':'not_uploaded.html'
+    , 'Complex Numbers and Quadratic Equations': 'not_uploaded.html'
+    , 'Linear Inequalities': 'not_uploaded.html'
+    , 'Permutations and Combinations': 'not_uploaded.html'
+    , 'Binomial Theorem': 'not_uploaded.html'
+    , 'Sequence and Series': 'not_uploaded.html'
+    , 'Straight Lines': 'not_uploaded.html'
+    , 'Conic Sections': 'not_uploaded.html'
+    , 'Introduction to Three-dimensional Geometry': 'not_uploaded.html'
+    , 'Limits and Derivatives': 'not_uploaded.html'
+    , 'Mathematical Reasoning': 'not_uploaded.html'
+    ,'Statistics':'not_uploaded.html'
+    ,'Probability':'not_uploaded.html'
+
+
+}
+Chemistry12={
+
+    '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+
+}
+
+
+Physics12 = {
+    '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+}
+
+Maths12 = {
+    '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+    , '': ''
+}
+symbol={
+'Chemistry':'H₂O'
+    ,'Physics': 'θ'
+    ,'Maths':'π'
+
+}
+
+
+
+Class_11 = {
+    'Chemistry':Chemistry11
+    ,'Physics': Physics11
+    ,'Maths':Maths11
+
+ }
+
+Class_12 ={
+    'Chemistry':Chemistry12
+    ,'Physics': Physics12
+    ,'Maths':Maths12
+}
+
+photo = {
+    'Chemistry': 'https://imgs.search.brave.com/DnA2nwwSpsHZavyEq7he-Hd9hHOHlFR3xHF-thDrrgI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/eXUuZWR1L3NpdGVz/L2RlZmF1bHQvZmls/ZXMvVGhpbmtzdG9j/a1Bob3Rvcy02NTgx/NDg4NDQuanBn'
+    ,'Physics': 'https://imgs.search.brave.com/Z2r75MakHlN-FvdkoXyl6WJDPasm6aqDMbN3OT7EOD8/rs:fit:1043:547:1/g:ce/aHR0cHM6Ly9wYnMu/dHdpbWcuY29tL21l/ZGlhL0RwSUhrd3lX/c0FJQ01fMi5qcGc6/bGFyZ2U'
+    ,'Maths':'https://imgs.search.brave.com/eQW4kU8zWOb7EZBMukL71xOzVEnABBEyh1Jc3mQMJbk/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cGR4LmVkdS9saWJl/cmFsLWFydHMtc2Np/ZW5jZXMvc2l0ZXMv/Zy9maWxlcy96bmxk/aHIxOTM2L2ZpbGVz/LzIwMjAtMDgvVUct/TWF0aGVtYXRpY3Mu/anBn'
+}
+
+
+
+
+
+
+from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        search = request.form.get('search-box').lower()
+        return render_template('search.html',search=search,Chemistry11=Chemistry11,Physics11=Physics11,Maths11=Maths11)
+
+
+    return render_template('index.html',Class_11=Class_11, photo=photo, symbol=symbol)
+
+
+
+@app.route('/not_uploaded.html')
+def no_upload():
+    return render_template('not_uploaded.html')
+
+
+
+# @app.route('/search' ,methods=['POST',"GET"])
+# def search():
+#     if request.method=='POST':
+#         search = request.form.get('search-box').lower()
+
+#         return render_template('search.html',search=search)
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000,debug=True)
