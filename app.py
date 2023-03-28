2c6401dbff65b3488d6f7003ea6eaeaa7c6ec4dd
Achiket123 <126181868+Achiket123@users.noreply.github.com>
2023-03-28 23:41:10 +0530

    final commit

--- app.py
+++ app.py
@@ -18,93 +18,102 @@ Chemistry11 = {
 
 }
 Physics11 = {
-    'Physical World': 'not_uploaded.html'
-    , 'Units and Measurements': 'not_uploaded.html'
-    , 'Motion in a Straight Line': 'not_uploaded.html'
-    , 'Motion in a Plane': 'not_uploaded.html'
-    , 'Laws of Motion': 'not_uploaded.html'
-    , 'Work, Energy and Power': 'not_uploaded.html'
-    , 'System of Particles and Rotational Motion': 'not_uploaded.html'
-    , 'Gravitation': 'not_uploaded.html'
-    , 'Mechanical Properties of Solids': 'not_uploaded.html'
-    , 'Mechanical Properties of Fluids': 'not_uploaded.html'
-    , 'Thermal Properties of Matter': 'not_uploaded.html'
-    , 'Thermodynamics': 'not_uploaded.html'
-    , 'Kinetic Theory': 'not_uploaded.html'
-    , 'Oscillations': 'not_uploaded.html'
-    , 'Waves': 'not_uploaded.html'
-    , 'Physical World': 'not_uploaded.html'
+    'Physical World': 'https://drive.google.com/file/d/1PrVT4bwBKBxBJR32Tupt_JXeE3yzRxiN/view?usp=sharing'
+    , 'Units and Measurements': 'https://drive.google.com/file/d/1uv2I8lbhxYF55XsbZRiumpgBn6rcjZML/view?usp=sharing'
+    , 'Motion in a Straight Line': 'https://drive.google.com/file/d/1-8cMjrqFkzEOUiwNJLXaCIe6hh-nv0E9/view?usp=sharing'
+    , 'Motion in a Plane': 'https://drive.google.com/file/d/1qjOvM2MA0lZ9XY5zSwkoS9ifaeLfvDY_/view?usp=share_link'
+    , 'Laws of Motion': 'https://drive.google.com/file/d/1w0yVPUyza5U4JAiwDezkwVKbgAsBtDCv/view?usp=sharing'
+    , 'Work, Energy and Power': 'https://drive.google.com/file/d/1KokvyJCifwOnv2csVwowiuuhNZxnCdR1/view?usp=sharing'
+    , 'System of Particles and Rotational Motion': 'https://drive.google.com/file/d/12tlXEGYcmFH8AUoAQpVGtVaXFuxvBVYK/view?usp=sharing'
+    , 'Gravitation': 'https://drive.google.com/file/d/1d0NmsgibJjFwkCeNAImYLiJPdYk3vXo5/view?usp=sharing'
+    , 'Mechanical Properties of Solids': 'https://drive.google.com/file/d/1XHyis0ke4EXjIoRflaHqD-zis-oSdjdy/view?usp=sharing'
+    , 'Mechanical Properties of Fluids': 'https://drive.google.com/file/d/1uzU44G9Qz8Sy-8CzUiYf_t46greQjhba/view?usp=sharing'
+    , 'Thermal Properties of Matter': 'https://drive.google.com/file/d/1zm9cnBlcfdzKKY2xc1AEkaiJ6ClyrtDg/view?usp=sharing'
+    , 'Thermodynamics': 'https://drive.google.com/file/d/1kKjB-BYWUHhUt_AlkY8FpGEEa1gahYzW/view?usp=sharing'
+    , 'Kinetic Theory': 'https://drive.google.com/file/d/1OEcSzbC4LgZJNPp2d6kOlsKF8OcZMdUH/view?usp=sharing'
+    , 'Oscillations': 'https://drive.google.com/file/d/1CrLzVsfISqA_kUZ1AfsU6bruVzKg8i5C/view?usp=sharing'
+    , 'Waves': 'https://drive.google.com/file/d/1CSN4poOxjPyLO-meDwh-asWki5rYRb7n/view?usp=sharing'
+    
 
 }
 Maths11 = {
-    'Sets': 'not_uploaded.html'
-    , 'Relations & Functions': 'not_uploaded.html'
-    , 'Principle of Mathematical Induction': 'not_uploaded.html'
-    , 'Trignometric Functions':'not_uploaded.html'
-    , 'Complex Numbers and Quadratic Equations': 'not_uploaded.html'
-    , 'Linear Inequalities': 'not_uploaded.html'
-    , 'Permutations and Combinations': 'not_uploaded.html'
-    , 'Binomial Theorem': 'not_uploaded.html'
-    , 'Sequence and Series': 'not_uploaded.html'
-    , 'Straight Lines': 'not_uploaded.html'
-    , 'Conic Sections': 'not_uploaded.html'
-    , 'Introduction to Three-dimensional Geometry': 'not_uploaded.html'
-    , 'Limits and Derivatives': 'not_uploaded.html'
-    , 'Mathematical Reasoning': 'not_uploaded.html'
-    ,'Statistics':'not_uploaded.html'
-    ,'Probability':'not_uploaded.html'
+    'Sets': 'https://drive.google.com/uc?authuser=0&id=0B2gZJF8gc6StM2ZrQ2NETHI1WEE&export=download'
+    , 'Relations & Functions': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StV29QVVhqVmNuRTQ'
+    , 'Principle of Mathematical Induction': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StRFRkV1Y4MEthX1k'
+    , 'Trignometric Functions':'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StSEU2eTVsdzdCTEk'
+    , 'Complex Numbers and Quadratic Equations': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StME0yNjIxa192Y28'
+    , 'Linear Inequalities': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StX0RwMXNROFlLTEk'
+    , 'Permutations and Combinations': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StcDRqUk5uOFJHSmc'
+    , 'Binomial Theorem': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StVXFsTXhCZlFHZjQ'
+    , 'Sequence and Series': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StSEVJOFM5MXh0Y3M'
+    , 'Straight Lines': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StOXlUVDUxZDZxSk0'
+    , 'Conic Sections': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StcDViS2w4S3dOZW8'
+    , 'Introduction to Three-dimensional Geometry': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StR3BuWG1UMFFCb0U'
+    , 'Limits and Derivatives': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StT3gzMTZnSDdOQlU'
+    , 'Mathematical Reasoning': 'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6Stdmt3TGV4aFV0UGM'
+    ,'Statistics':'https://docs.google.com/uc?export=download&id=0B2gZJF8gc6StZmJ5ZDdHWUtGd3M'
+    ,'Probability':'https://drive.google.com/uc?export=download&id=0B2gZJF8gc6StQ3hlaHZmWWVDQ2c'
 
 
 }
 Chemistry12={
+    'The Solid States':'https://drive.google.com/file/d/1tKnTpZGADBWJqub_pSe-d1nPGgaNyhHS/view?usp=sharing',
 
-    '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
+    'Solutions': 'https://drive.google.com/file/d/16KZ1NQQMAbswGL7daxhqGw0hxYWVaWyN/view?usp=sharing'
+    , 'Electrochemistry': 'https://drive.google.com/file/d/1fy9W7LYkByDm9F_oMTw2CqlAthtVPHkk/view?usp=sharing'
+    , 'Chemical Kinetics': 'https://drive.google.com/file/d/1OxlrMkpVuQuV09bO0y3eTi5UkskaRuLh/view?usp=sharing'
+    ,'Surface Chemistry':'https://drive.google.com/file/d/1vNAQjzXgfTnAY8fOIEBj0SZYCIjpJQhg/view?usp=sharing'
+    ,'Metallurgy':'https://drive.google.com/file/d/1cdlnLUVSQLDBpKpc8uEFCzwBTePGFJZU/view?usp=sharing'
+    ,'The p -block Elelments':'https://drive.google.com/file/d/1_4byufOg0nYFeDSsswnoWBproBXchrVm/view?usp=sharing'    
+    , 'd -and f -Block Elements': 'https://drive.google.com/file/d/1c8ULz8bAPjsj_ld57eNosQio96KYZNhi/view?usp=sharing'
+    , 'Coordination Compounds': 'https://drive.google.com/file/d/1TmwjuJk1M9N2ORvTEZOQ9bN16_2sfzfQ/'
+    , ' Haloalkanes and Haloarenes': 'https://drive.google.com/file/d/1ecuqieAA82IBeRsdt5gn2qhlmRsDtNTU/view?usp=sharing'
+    , 'Alcohols, Phenols and Ethers': 'https://drive.google.com/file/d/1S_y7IypH5n7El-VxudyFHaQJz1kiqwtq/view?usp=sharing'
+    , 'Aldehydes, Ketones and Carboxylic Acids': 'https://drive.google.com/file/d/163B1TGDDV3tcWt4Hqxrfln0DJnReoTyK/view?usp=sharing'
+    , 'Amines': 'https://drive.google.com/file/d/1gN9ddzqUg3AWnCMPSriOhB4qfHGPU-WT/view?usp=sharing'
+    , 'Biomolecules': 'https://drive.google.com/file/d/1bPn1FmXYhnVHfOOcsmId7xtw_gV0ZTjz/view?usp=sharing'
+    ,'Polymers':'https://drive.google.com/file/d/1PUKEBhtdUPH8qEbnTllqaaPmCo-WiLiw/view?usp=sharing'
+    ,'Chemistry in Everyday Life':'https://drive.google.com/file/d/1WEgSlfW3mx2JYhYjpYO0sCwWTw4c2aak/view?usp=sharing'
+    
 
 }
-
+quote={
+    'Class 11':'Welcome to Narak',
+    'Class 12':'Abhi tk jinda ho!!!'
+}
 
 Physics12 = {
-    '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
+    'Electric Charges and Fields': 'https://drive.google.com/file/d/1f9Q7DR-Nh_i95hsOb0zMgs4hTPnfMcBI/view?usp=sharing'
+    , 'Electrostatic Potential andCapacitance': 'https://drive.google.com/file/d/1Y0gHkVJ3xRMlNReL4Iu2t0ieiMpCKpJ4/view?usp=sharing'
+    , 'Current Electricity': 'https://drive.google.com/file/d/1LU48KgUzZzwl1KoDJ8p8L8Tc62kHbg8z/view?usp=sharing'
+    , 'Moving Charges and Magnetism': 'https://drive.google.com/file/d/1IbyDYIDTj_VtCwJPJMwmve-DT7gywzxI/view?usp=sharing'
+    , 'Magnetism and Matter': 'https://drive.google.com/file/d/1j-SJroAKFGSj24YT_21Tsn9wRfFQUGRC/view?usp=sharing'
+    , 'Electromagnetic Induction': 'https://drive.google.com/file/d/1dc7bDsLHHuHQ0Z6MINjsMAYTubo62kSZ/view?usp=sharing'
+    , 'Alternating Current': 'https://drive.google.com/file/d/1m_MovZ8E70xMC4Gt7_8oYuwMQNYZ009d/view?usp=sharing'
+    , 'Electromagnetic Waves': 'https://drive.google.com/file/d/1JhOJxKZQC9PQ8vsdkQEvkUNTFY-4PG48/view?usp=sharing'
+    , 'Ray Optics and Optical Instruments': 'https://drive.google.com/file/d/1DNVM-k6JS0iexIWFeSupI4DMGaJgiuLH/view?usp=sharing'
+    , 'Wave Optics': 'https://drive.google.com/file/d/1wDRxj_Vh3jX70NlJS7Ck8SziuLjBhb0e/view?usp=sharing'
+    , 'Dual Nature of Radiation and Matter': 'https://drive.google.com/file/d/1mWSPnEd_UN7CAQPjib_N5J9l6PuGoGWq/view?usp=sharing'
+    , 'Atoms': 'https://drive.google.com/file/d/1zfyLc979CuQAzmNTAKvMq2YtHWqNDa_D/view?usp=sharing'
+    , 'Nuclei': 'https://drive.google.com/file/d/1bAQOQFbZwpZeCPs6Re-m5gmwSXBiHCVC/view?usp=sharing'
+    , 'Semiconductor Electronics: Materials, Devices and Simple Circuits': 'https://drive.google.com/file/d/1bAQOQFbZwpZeCPs6Re-m5gmwSXBiHCVC/view?usp=sharing'
 }
 
 Maths12 = {
-    '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
-    , '': ''
+    'Relations and Functions': 'https://drive.google.com/open?id=11w0VHtru1xIXigLIDP0T_LPb6ETm3jiH'
+    , 'Inverse Trigonometric Functions': 'https://drive.google.com/open?id=15JytlbNtNUqeBIUAFcCjRX3l5aJqJcVc'
+    , 'Matrices': 'https://drive.google.com/open?id=1RG-j4riSu7DlSMuq8bI6QwlCUOylDwPv'
+    , 'Determinants': 'https://drive.google.com/open?id=1xK2BYv4c2urlJoGWnlw0in03uiSRt3Ft'
+    , 'Continuity and Differentiability': 'https://drive.google.com/open?id=12kTP3OSdQm_xaMh1wheK0OQdUbksED4e'
+    , 'Applications of Derivatives': 'https://drive.google.com/open?id=1ZoMrY297iJl-cPtw9bfnYRSxGkSBLIk5'
+    , 'Integrals': 'https://drive.google.com/file/d/1o8Qi7bY68McZoPCTbxvsK-4hTGZbQR_N/view?usp=share_link'
+    , 'Applications of the Integrals': 'https://homepage.divms.uiowa.edu/~stroyan/CTLC3rdEd/3rdCTLCText/Chapters/Ch14.pdf'
+    , 'Differential Equations': 'https://drive.google.com/open?id=1c79TPGNzt5Qxw_4J9cKNho85UefbYkbS'
+    , 'Vectors': 'https://drive.google.com/open?id=12xpC-6UsUMld8fNQigLxYZ0FFU2bZ5IZ'
+    , 'Three – dimensional Geometry': 'https://drive.google.com/open?id=1NCJfaffFR3ZEsmXFLeUfocW3NWA6GeBm'
+    , 'Linear Programming': 'https://drive.google.com/open?id=1ggnmH4wR2OlPqRy15JJikMvaidM7Uir_'
+    , 'Probability': 'https://drive.google.com/open?id=1GNIiqx44oM92fENG3VnaJQcRQ6AnRSKi'
+
 }
 symbol={
 'Chemistry':'H₂O'
@@ -128,6 +137,11 @@ Class_12 ={
     ,'Maths':Maths12
 }
 
+Class = {
+    'Class 11': Class_11
+    ,'Class 12':Class_12
+}
+
 photo = {
     'Chemistry': 'https://imgs.search.brave.com/DnA2nwwSpsHZavyEq7he-Hd9hHOHlFR3xHF-thDrrgI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/eXUuZWR1L3NpdGVz/L2RlZmF1bHQvZmls/ZXMvVGhpbmtzdG9j/a1Bob3Rvcy02NTgx/NDg4NDQuanBn'
     ,'Physics': 'https://imgs.search.brave.com/Z2r75MakHlN-FvdkoXyl6WJDPasm6aqDMbN3OT7EOD8/rs:fit:1043:547:1/g:ce/aHR0cHM6Ly9wYnMu/dHdpbWcuY29tL21l/ZGlhL0RwSUhrd3lX/c0FJQ01fMi5qcGc6/bGFyZ2U'
@@ -146,16 +160,21 @@ app=Flask(__name__)
 def home():
     if request.method=='POST':
         search = request.form.get('search-box').lower()
-        return render_template('search.html',search=search,Chemistry11=Chemistry11,Physics11=Physics11,Maths11=Maths11)
+        return render_template('search.html',search=search,Chemistry11=Chemistry11,Physics11=Physics11,Maths11=Maths11,Chemistry12=Chemistry12,Physics12=Physics12,Maths12=Maths12)
 
 
-    return render_template('index.html',Class_11=Class_11, photo=photo, symbol=symbol)
+    return render_template('index.html',Class_11=Class_11, photo=photo, symbol=symbol,Class=Class,quote=quote)
 
 
 
-@app.route('/not_uploaded.html')
+@app.route('/' ,methods=['POST','GET'])
 def no_upload():
-    return render_template('not_uploaded.html')
+    if request.method=='POST':
+        search = request.form.get('search-box').lower()
+        return render_template('search.html',search=search,Chemistry11=Chemistry11,Physics11=Physics11,Maths11=Maths11,Chemistry12=Chemistry12,Physics12=Physics12,Maths12=Maths12)
+
+
+    return render_template('')
 
 
 
